6bd2a6235bba646dc7e252cfad8797a7d09b5dc1
jones <tansong0091@yeah.net>
2021-10-04 17:31:49 +0800

    add

--- Fargate_SSO/entrypoint.sh
+++ Fargate_SSO/entrypoint.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 #/usr/local/apache2/bin/httpd -f /usr/local/apache2/conf/httpd.conf -k start
 #when using apachectl it take environment varianles in /usr/local/apache2/bin/envvars but when using httpd start directly it will not use env in envvars
 HOST=`hostname`
 echo "export HOSTNAME=$HOST" >>/usr/local/apache2/bin/envvars
 /usr/local/apache2/bin/apachectl start
+tail -f /dev/null
 
 
